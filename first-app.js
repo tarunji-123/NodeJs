e105922bd5901a61139900b875cce3e4e491ffb2
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-06-29 02:55:58 +0530

    url hits

--- first-app.js
+++ first-app.js
@@ -2,9 +2,36 @@
 
 const http = require('http')
 
+
 const server = http.createServer((req,res)=>{
-    console.log("Tarun Bhadoriya");
-    res.end("Hello, Tarun");
+    // console.log("Tarun Bhadoriya");
+    // res.end("Hello, Tarun");
+    const url = req.url;
+    if(url === '/home'){
+        res.write('<html>');
+        res.write('<head><title>My first Page</title><head>');
+        res.write('<body><h1>Welcome to Home Page</h1></body>');
+        res.write('<html');
+        res.end();
+    }
+    else if(url === '/about'){
+        res.write('<html>');
+        res.write('<head><title>My first Page</title><head>');
+        res.write('<body><h1>Welcome to About us Page</h1></body>');
+        res.write('<html');
+        res.end();
+    }
+    else if(url === '/node'){
+        res.write('<html>');
+        res.write('<head><title>My first Page</title><head>');
+        res.write('<body><h1>Welcome to Node js Project </h1></body>');
+        res.write('<html');
+        res.end();
+    }
+    else{
+        res.end("go to any other url");
+    }
+
 })
 
 const port = 4000;
