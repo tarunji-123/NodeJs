10362a11ac06ced09edad6f74960642304e28e33
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-06-28 17:28:59 +0530

    create first server

--- first-app.js
+++ first-app.js
@@ -1,1 +1,13 @@
-console.log("Hello World from Nodejs");
+// console.log("Hello World from Nodejs");
+
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+    console.log("Tarun Bhadoriya");
+    res.end("Hello, Tarun");
+})
+
+const port = 4000;
+server.listen(port, ()=>{
+    console.log(`Server running on ${port}`);
+})
