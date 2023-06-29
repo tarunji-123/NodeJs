c29c6e9ac8d62726f4ac90a1d02c12315b5f21c8
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-06-30 00:28:50 +0530

    add routes and make admin and shop file

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-const http = require('http');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
-});
+// Filtering use on adminRoutes
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>');
 })
 
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello from Express! </h1>');
-})
-
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
