d9ad1d6d2cec5aaa0bd3512728f3efbcbe213dd0
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-06-29 18:35:17 +0530

    add add-product

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res, next)=>{
-    console.log("In the middleware");
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
 
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
 })
-app.use((req,res,next)=>{
-    console.log("another middleware");
-    // res.send('<h1>Hello From Express Js</h1>');
-    res.send( { key1: 6 });
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello from Express! </h1>');
 })
 
 const server = http.createServer(app);
