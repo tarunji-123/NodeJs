f03ffe06d7e087756e479a038a0839fc8dc0ec0b
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-07-05 11:04:30 +0530

    use model

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 
-const productController = require('../controllers/product');
+const productController = require('../controllers/products');
 
 
 
