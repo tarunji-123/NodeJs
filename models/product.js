76dcb6988b5dd67f9d3c791d3c1be23699c264e2
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-07-12 17:45:18 +0530

    many to many relation

--- models/product.js
+++ models/product.js
@@ -1,31 +1,27 @@
 const Sequelize = require('sequelize');
+
 const sequelize = require('../util/database');
 
-//now define a model that will be manage by sequelize
-
-const Product = sequelize.define('product',{
-  id : {
-    type : Sequelize.INTEGER,
-    autoIncrement : true,
-    allowNull : false,
-    primaryKey : true
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
   },
-
-  title : Sequelize.STRING,
-  price:{
-    type : Sequelize.DOUBLE,
-    allowNull : true,
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
   },
-
-  imageUrl : {
-    type : Sequelize.STRING,
-    allowNull : false
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
   },
-
-  description : {
-    type : Sequelize.STRING,
-    allowNull : false
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
-})
+});
 
 module.exports = Product;
